9a5c3f48459df5a4a7819acf60441b07eae8fb67
DATMP <a2021102789@uwiener.edu.pe>
2022-09-14 20:58:21 -0500

    clase4x

---
+++ Clase4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Reyner!");
+
+int nNumero = 0;
+Console.WriteLine("ingrese un numero");
+nNumero = int.Parse(Console.ReadLine());
+
+if (nNumero > 12)
+    Console.WriteLine("El numero es mayor a 12");
+else
+    Console.WriteLine("El numero es menor o igual a 12");
+Thread.Sleep(9000);
